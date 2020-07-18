fe56f2e02afd687aa1a700c70b987d73f48a43b0
EgorShilov1987 <egas1987@mail.ru>
2020-07-18 09:14:51 +0300

    Test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,42 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args){
+        float a = 10;float b = 30; float c = 10;float d = 30;
+        float x = a * (b + (c / d));
+        System.out.println(x);
+    }
+
+    public static boolean task10and20(int x1, int x2){
+        if(x1 >= 10 && x2 <= 20){
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public static void isPositiveOrNegative(int x) {
+        if(x >= 0) {
+            System.out.println("Это положительное число.");
+        } else {
+            System.out.println("Это отрицательное число.");
+        }
+    }
+    public static boolean isNegative(int x) {
+        if(x<0) {
+            return true;
+        }
+        return false;
+    }
+    public static void greetings(String name) {
+        System.out.println("Привет! " + name);
+    }
+    public static void year(int i) {      //Не совсем понятно как правильно написать условия!!!
+        if(i%4 == 0 && i%100 != 0 && i%400 == 0) {
+            System.out.println("Это високосный год.");
+        } else {
+            System.out.println("Это не високосный год");
+        }
     }
 }
+
+
